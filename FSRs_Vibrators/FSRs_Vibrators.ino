ebc39e0c368f2451e838dc89cbe942fbf139882a
SamSD <ss15060@my.bristol.ac.uk>
2019-11-26 18:46:36 +0000

    Fixed scaling for vibration motors

--- FSRs_Vibrators/FSRs_Vibrators.ino
+++ FSRs_Vibrators/FSRs_Vibrators.ino
@@ -5,7 +5,8 @@
 
 #define f4Sense A0     // the number of the pushbutton pin
 #define f4Vib 6     // the number of the vib 2 pin
-// variables will change:
+
+
 float f3Pressure = 0;         // variable for reading the FSR status
 float f4Pressure = 0;         // variable for reading the FSR status
 float f3Force = 0;
@@ -24,48 +25,27 @@ void setup(){
 
 void loop() {
   // read the state of the pushbutton value:
- float f3output = sense(f3Sense,f3Vib);
- float f4output = sense(f4Sense,f4Vib);
+  const float f3output = sense(f3Sense,f3Vib);
+  const float f4output = sense(f4Sense,f4Vib);
 
   analogWrite(f3Vib, f3output);
   analogWrite(f4Vib, f4output);
 
+  Serial.print("f3: ");
   Serial.print(f3output);
   Serial.print(",");
-  Serial.print(analogRead(f3Sense));
+  Serial.print("f3output: ");
+  Serial.print(f3output);
   Serial.print(",");
-
+  Serial.print("f4: ");
   Serial.print(f4output);
   Serial.print(",");
-  Serial.print(analogRead(f4Sense));
-  Serial.print(",");
-  Serial.println(153);
-  }
+  Serial.print("f4output: ");
+  Serial.print(f4output);
+  Serial.println();
+}
 
-
-
-  
-
-//  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-//  if (button1State == HIGH) {
-//    // vibrate motor
-//    analogWrite( vib1Pin , 153 );
-//  } else {
-//    // turn  off motor
-//    analogWrite( vib1Pin , 0 );
-//  }
-//
-//  if (button2State == HIGH) {
-//    // vibrate motor
-//    analogWrite( vib2Pin , 153 );
-//  } else {
-//    // turn  off motor
-//    analogWrite( vib2Pin , 0 );
-//  }
-//}
-
-float sense(int pressurePin, int vibrationPin) {
-  float pressure = analogRead(pressurePin);
-  Serial.print(exp(pressure));
-  return (pressure*153)/1000; 
+float sense(const int& pressurePin, const int& vibrationPin) {
+  const float pressure = analogRead(pressurePin);
+  return exp(pressure/200); 
 }
